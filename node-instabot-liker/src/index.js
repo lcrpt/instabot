38cec61d5c67ca5782b907960460490031f2c3f2
leolassence <leolassence@gmail.com>
2018-05-15 11:51:53 +0200

    Fix liker cron

--- node-instabot-liker/src/index.js
+++ node-instabot-liker/src/index.js
@@ -1,5 +1,23 @@
 require('dotenv').load();
-
+const moment = require('moment');
 const { jobRunner, computeTimer } = require('./job');
 
-jobRunner(computeTimer());
+const FORMAT = 'hh:mm:ss';
+const BEFORE_TIME = moment('08:00:00', FORMAT);
+const AFTER_TIME = moment('20:00:00', FORMAT);
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+(async () => {
+    while (true) {
+        const time = moment(moment().format(FORMAT), FORMAT);
+
+        if (time.isBetween(BEFORE_TIME, AFTER_TIME)) {
+            await jobRunner(computeTimer());
+        } else {
+            await sleep(60000);
+        }
+    }
+})();
